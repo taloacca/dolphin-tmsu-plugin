44b1325bcdc82af1b61002c876f8d78bdaec9c73
taloacca <taloacca@gmail.com>
2022-02-17 22:16:51 -0700

    Crappy autocomplete based on tags that exist

--- src/tagdialog.cpp
+++ src/tagdialog.cpp
@@ -3,6 +3,8 @@
 #include <QString>
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QLineEdit>
+#include <QCompleter>
 
 TagDialog::TagDialog(const QMap< QString, QList< TMSUTag > > &fileTagMap, const QMap< QString, int > &tagInfoMap, QWidget* parent) :
     QDialog(parent)
@@ -13,6 +15,26 @@ TagDialog::TagDialog(const QMap< QString, QList< TMSUTag > > &fileTagMap, const 
     QVBoxLayout *mainLayout = new QVBoxLayout;
     this->setLayout(mainLayout);
     mainLayout->addWidget(mainWidget);
+
+    // TODO: need to decide how to handle multiple sets of tags on multiple files.  Show common tags only?
+
+    QList< QString > tagList;
+    for(auto it = tagInfoMap.keyValueBegin(); it != tagInfoMap.keyValueEnd(); ++it)
+    {
+        tagList.append(it->first);
+    }
+
+
+    QCompleter *completer = new QCompleter(tagList);
+    completer->setModelSorting(QCompleter::CaseInsensitivelySortedModel);
+    completer->setCompletionMode(QCompleter::PopupCompletion);
+    // completer->setFilterMode(Qt::MatchContains);
+
+    QLineEdit *newTagName = new QLineEdit();
+    newTagName->setMinimumWidth(150);
+    newTagName->setClearButtonEnabled(true);
+    newTagName->setCompleter(completer);
+    mainLayout->addWidget(newTagName);
 
     for(auto it = fileTagMap.keyValueBegin(); it != fileTagMap.keyValueEnd(); ++it)
     {
